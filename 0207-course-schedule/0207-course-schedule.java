6f2b0776950444bcfd4f68507125b695bc020306
Keshav Rao Maratha <2024btechaidskeshav17932@poornima.edu.in>
2026-09-19 09:32:58 +0530

    Time: 6 ms (72.46%), Space: 46.9 MB (77.23%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        ArrayList<Integer>[] graph = new ArrayList[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        int[] indegree = new int[numCourses];
+
+        // Build graph
+        for (int[] edge : prerequisites) {
+
+            int course = edge[0];
+            int prerequisite = edge[1];
+
+            graph[prerequisite].add(course);
+            indegree[course]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        // Add courses with indegree 0
+        for (int i = 0; i < numCourses; i++) {
+
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        int count = 0;
+
+        // Topological Sort (BFS)
+        while (!queue.isEmpty()) {
+
+            int current = queue.remove();
+            count++;
+
+            for (int neighbor : graph[current]) {
+
+                indegree[neighbor]--;
+
+                if (indegree[neighbor] == 0) {
+                    queue.add(neighbor);
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
+}
